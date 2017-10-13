9958ada0a6043783f6ec8bda5b38e3261987c910
jhdevteam <andrew_paw@manulife.com>
2017-10-13 07:23:13 +0530

    First commit

---
+++ index.ios.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import JHShuttleMap from "./Source/MapHome";
+export default class JHShuttleServiceApp extends Component {
+  render() { 
+     return (
+         <JHShuttleMap {...this.props}/>       
+      );
+  }
+}
+
+AppRegistry.registerComponent('JHShuttleServiceApp', () => JHShuttleServiceApp);
